1c3dfca362ab8bfc2353425a12cd35d8540fbab6
arkadyt <contact.me@arkadyt.dev>
2019-03-10 14:14:09 -0700

    Add db-connection.js

---
+++ server/db-connection.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const { MONGO_URI } = require('./keys/keys.dev.js')
+
+/**
+ * Attempts to connect to MongoDB.
+ */
+function connectToDB(callback) {
+  const db = mongoose.connection
+  db.on('connecting', () => console.log('Connecting to database...'))
+  db.on('open', () => {
+    console.log('Connected to database.')
+    if (callback) callback()
+  })
+  db.on('error', err => {
+    console.log('Error in database connection:\n\t' + err)
+    mongoose.disconnect()
+    console.log('Attempting to reconnect in 5 seconds...')
+  })
+  db.on('disconnected', () => {
+    new Promise(resolve => setTimeout(resolve, 5000)).then(() => {
+      mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+    })
+  })
+  mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+}
+
+module.exports = {
+  connectToDB
+}
