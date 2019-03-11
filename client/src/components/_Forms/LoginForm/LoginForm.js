8ca27f9611749e65e3f3d517ec3fb84bb0a9c141
arkadyt <contact.me@arkadyt.dev>
2019-03-11 00:29:17 -0700

    Complete LoginForm

--- client/src/components/_Forms/LoginForm/LoginForm.js
+++ client/src/components/_Forms/LoginForm/LoginForm.js
@@ -1,3 +1,18 @@
 import React from 'react'
 
-export default () => <div>Login Form</div>
+import Field from '../../Field/Field'
+import './LoginForm.scss'
+
+export default () => {
+  return (
+    <div className="LoginForm-container">
+      <h1>Please login</h1>
+      <p>To proceed, please login by entering your name in the box below:</p>
+      <Field
+        placeholder="eg. Arnold" />
+      <Field
+        type="button"
+        value="Log in" />
+    </div>
+  )
+}
