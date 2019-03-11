a814f50a8e6d7c39a5d2030398b172b8e6a2b61f
arkadyt <contact.me@arkadyt.dev>
2019-03-10 17:04:39 -0700

    Rewrite App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import NavBar from './components/NavBar/NavBar.js'
+import Root from './components/Root/Root.js'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Root />
+    </div>
+  )
 }
 
-export default App;
+export default App
