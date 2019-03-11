33bc2ccc473be92576b6770ab2b9a95f354bbd0e
arkadyt <contact.me@arkadyt.dev>
2019-03-10 20:49:54 -0700

    Setup routing

--- client/src/components/RootLayout/RootLayout.js
+++ client/src/components/RootLayout/RootLayout.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router'
-import { connect } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
 
 import HomePage from '../_Pages/HomePage/HomePage'
 import ProductViewPage from '../_Pages/ProductViewPage/ProductViewPage'
+import NotFoundPage from '../_Pages/NotFoundPage/NotFoundPage'
+import PrivateRoute from '../PrivateRoute/PrivateRoute'
+import LoginForm from '../_Forms/LoginForm/LoginForm'
 import "./RootLayout.scss"
 
 class RootLayout extends Component {
@@ -11,18 +13,14 @@ class RootLayout extends Component {
     return (
       <div className="RootLayout-container">
         <Switch>
-          <Route path="/home" component={HomePage} />
-          <Route path="/product/:productId" component={ProductViewPage} />
+          <PrivateRoute exact path="/" component={HomePage} />
+          <PrivateRoute path="/product/:productId" component={ProductViewPage} />
+          <Route path="/login" component={LoginForm} />
+          <Route component={NotFoundPage} />
         </Switch>
       </div>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  products: state.products
-})
-
-export default connect(
-  mapStateToProps
-)(RootLayout)
+export default RootLayout
