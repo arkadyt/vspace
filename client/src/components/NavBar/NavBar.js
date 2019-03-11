8b69eed509cb570d9b58b0f04c9e2249160c8989
arkadyt <contact.me@arkadyt.dev>
2019-03-10 21:54:21 -0700

    Style NavBar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './NavBar.scss'
 
 const NavBar = () => {
   return (
-    <div>I am a NavBar!</div>
+    <div className="NavBar-container">
+      <NavLink to="/" className="NavBar-navlink justify-start" activeClassName="active">
+        Home
+      </NavLink>
+      <span className="NavBar-item">Welcome, %userName%</span>
+      <NavLink to="/users" className="NavBar-navlink" activeClassName="active">
+        View all users
+      </NavLink>
+    </div>
   )
 }
 
