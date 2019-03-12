0292b509f0a76512dc24cfe6f26e2facf3da3257
arkadyt <contact.me@arkadyt.dev>
2019-03-11 19:44:31 -0700

    Setup axios

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = "http://localhost:5501"
+} else {
+  axios.defaults.baseURL = "https://api.arkadyt.com/hp"
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
+serviceWorker.unregister()
