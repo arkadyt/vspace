78308e92036cd398ade59585f7526e54cb27231c
arkadyt <contact.me@arkadyt.dev>
2019-03-11 18:02:03 -0700

    Setup redux-persist

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import RootLayout from './components/RootLayout/RootLayout.js'
 import NavBar from './components/NavBar/NavBar.js'
-import store from './state/store.js'
+import { store, persistor } from './state/store.js'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <RootLayout />
-        </Fragment>
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router>
+          <Fragment>
+            <NavBar />
+            <RootLayout />
+          </Fragment>
+        </Router>
+      </PersistGate>
     </Provider>
   )
 }
--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,6 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/_root'
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 let store
 const middleware = [thunk]
@@ -8,9 +16,11 @@ const initialState = {}
 
 if (process.env.NODE_ENV === 'development') {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)))
+  store = createStore(persistedReducer, initialState, composeEnhancers(applyMiddleware(...middleware)))
 } else {
-  store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)))
+  store = createStore(persistedReducer, initialState, compose(applyMiddleware(...middleware)))
 }
 
-export default store
+let persistor = persistStore(store)
+
+export { store, persistor }
