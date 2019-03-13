fcb162eed33b5055a2db59fd957fc4a49bb22aa2
arkadyt <contact.me@arkadyt.dev>
2019-03-12 17:45:02 -0700

    Add new rewriter route

--- server/api/product.js
+++ server/api/product.js
@@ -4,6 +4,7 @@ const server = jsonServer.create()
 const router = jsonServer.router(global.__basedir + '/data/test_data.json')
 
 const rewriter = jsonServer.rewriter({
+  '/products?_sort=*&_order=*': '/data?_sort=$1&_order=$2',
   '/products/*': '/data?product_id=$1',
   '/products': '/data'
 })
