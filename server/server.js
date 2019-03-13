a1caeae812c1b0b0c296c3bcb6e7f88939b4ed6e
arkadyt <contact.me@arkadyt.dev>
2019-03-12 18:20:03 -0700

    Setup new /users router

--- server/server.js
+++ server/server.js
@@ -2,12 +2,18 @@ global.__basedir = __dirname;
 
 const express = require('express')
 const { jsonServer, jsonServerRewriter } = require('./api/product.js')
+const userRouter = require('./api/user.js')
+const bodyParser = require('body-parser')
 const dbConnection = require('./db-connection.js')
 const cors = require('./config/cors.policy.js')
 
 const app = express()
 app.use(cors)
 app.use(jsonServerRewriter)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/users', userRouter)
 app.use(jsonServer)
 
 dbConnection.connectToDB(() => {
