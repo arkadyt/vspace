93ae774ca51702b4280097280a3b262216a0ff0e
arkadyt <contact.me@arkadyt.dev>
2019-12-30 12:04:16 -0800

    Update api base

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker'
 import axios from 'axios'
 
 if (process.env.NODE_ENV === 'development') {
-  axios.defaults.baseURL = "http://localhost:5501"
+  axios.defaults.baseURL = "http://localhost:5000"
 } else {
-  axios.defaults.baseURL = "https://api.arkadyt.com/hp"
+  axios.defaults.baseURL = "https://api.arkadyt.com/vspace"
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
